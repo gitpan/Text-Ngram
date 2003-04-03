b773c37f22db4be46c669a9b946bd0102c28cfb3
Simon Cozens <simon@cpan.org>
2003-04-03 11:09:27 +0000

    Import of SIMON/Text-Ngram-0.02 from CPAN.
    gitpan-cpan-distribution: Text-Ngram
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SIMON/Text-Ngram-0.02.tar.gz
    gitpan-cpan-author:       SIMON
    gitpan-cpan-maturity:     released

--- Ngram.pm
+++ Ngram.pm
@@ -7,11 +7,11 @@ use warnings;
 require Exporter;
 
 our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [ qw( count add_to_counts) ] );
+our %EXPORT_TAGS = ( 'all' => [ qw( ngram_counts add_to_counts) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 require XSLoader;
 XSLoader::load('Text::Ngram', $VERSION);
